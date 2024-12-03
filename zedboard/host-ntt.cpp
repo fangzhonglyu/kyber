a51f6772c07c6e3071c6365a114fc161d21a0b55
acm289 <acm289@cornell.edu>
2024-12-03 12:01:35 -0500

    Maybe better testing?

--- zedboard/host-ntt.cpp
+++ zedboard/host-ntt.cpp
@@ -12,6 +12,9 @@
 #include <stdio.h>
 #include <string.h>
 #include "top.h"
+#include "params.h"
+#include "typedefs.h"
+#include "randombytes.h"
 
 #define NTESTS 10
 #define ANSI_COLOR_RED     "\x1b[31m"
@@ -31,7 +34,27 @@
   printf("\n");
 #endif
 
-void ntt(sbit16_t r[256], int fdr, int fdw) {
+const sbit16_t zetas[128] = {
+    -1044, -758,  -359,  -1517, 1493,  1422,  287,   202,  -171,  622,   1577,
+    182,   962,   -1202, -1474, 1468,  573,   -1325, 264,  383,   -829,  1458,
+    -1602, -130,  -681,  1017,  732,   608,   -1542, 411,  -205,  -1571, 1223,
+    652,   -552,  1015,  -1293, 1491,  -282,  -1544, 516,  -8,    -320,  -666,
+    -1618, -1162, 126,   1469,  -853,  -90,   -271,  830,  107,   -1421, -247,
+    -951,  -398,  961,   -1508, -725,  448,   -1065, 677,  -1275, -1103, 430,
+    555,   843,   -1251, 871,   1550,  105,   422,   587,  177,   -235,  -291,
+    -460,  1574,  1653,  -246,  778,   1159,  -147,  -777, 1483,  -602,  1119,
+    -1590, 644,   -872,  349,   418,   329,   -156,  -75,  817,   1097,  603,
+    610,   1322,  -1285, -1465, 384,   -1215, -136,  1218, -1335, -874,  220,
+    -1187, -1659, -1185, -1530, -1278, 794,   -1510, -854, -870,  478,   -108,
+    -308,  996,   991,   958,   -1460, 1522,  1628};
+
+sbit16_t montgomery_reduce(sbit32_t a);
+sbit16_t fqmul(sbit16_t a, sbit16_t b) {
+  return montgomery_reduce( (sbit32_t) a * b );
+}
+sbit16_t barrett_reduce(sbit16_t a);
+
+void ntt_stream(sbit16_t r[256], int fdr, int fdw) {
   // Write select word
   bit32_t sel = 0;
   int nbytes = write(fdw, (void *)&sel, sizeof(sel));
@@ -57,7 +80,7 @@ void ntt(sbit16_t r[256], int fdr, int fdw) {
     poly[i + 1] = output_word( 31, 16 );
   }
 }
-void invntt(sbit16_t r[256], int fdr, int fdw) {
+void invntt_stream(sbit16_t r[256], int fdr, int fdw) {
   // Write select word
   bit32_t sel = 1;
   int nbytes = write(fdw, (void *)&sel, sizeof(sel));
@@ -156,12 +179,12 @@ static int test_ntt(int fdr, int fdw)
     r[i](15, 0) = r_gold[i];
   }
 
-  ntt(r, fdr, fdw);
+  ntt_stream(r, fdr, fdw);
   ntt_gold(r_gold);
 
   for (int i = 0; i < 256; i++) {
     if (r[i] != r_gold[i]) {
-      printf(ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET " ntt[%d] = %d, ntt_gold[%d] = %d\n", i, r[i], i, r_gold[i]);
+      printf(ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET " ntt[%d] = %d, ntt_gold[%d] = %d\n", i, int(r[i]), i, int(r_gold[i]));
       return 1;
     }
   }
@@ -185,12 +208,12 @@ static int test_invntt(int fdr, int fdw)
     r[i](15, 0) = r_gold[i];
   }
 
-  invntt(r, fdr, fdw);
+  invntt_stream(r, fdr, fdw);
   invntt_gold(r_gold);
 
   for (int i = 0; i < 256; i++) {
     if (r[i] != r_gold[i]) {
-      printf(ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET " ntt[%d] = %d, ntt_gold[%d] = %d\n", i, r[i], i, r_gold[i]);
+      printf(ANSI_COLOR_RED "ERROR" ANSI_COLOR_RESET " ntt[%d] = %d, ntt_gold[%d] = %d\n", i, int(r[i]), i, int(r_gold[i]));
       return 1;
     }
   }
