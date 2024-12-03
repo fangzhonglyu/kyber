e2495ab22d1c6b0d9a9f3aa2b1a47bd19942fb35
acm289 <acm289@cornell.edu>
2024-12-03 11:55:46 -0500

    Updated testing

--- zedboard/host-ntt.cpp
+++ zedboard/host-ntt.cpp
@@ -1,11 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <math.h>
+#include <assert.h>
+
+#include <iostream>
+#include <fstream>
+
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
-#include "../params.h"
-#include "../typedefs.h"
-#include "../randombytes.h"
+#include "top.h"
 
-#define NTESTS 100
+#define NTESTS 10
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
@@ -23,28 +31,58 @@
   printf("\n");
 #endif
 
+void ntt(sbit16_t r[256], int fdr, int fdw) {
+  // Write select word
+  bit32_t sel = 0;
+  int nbytes = write(fdw, (void *)&sel, sizeof(sel));
+  assert(nbytes == sizeof(sel));
 
-const sbit16_t zetas[128] = {
-    -1044, -758,  -359,  -1517, 1493,  1422,  287,   202,  -171,  622,   1577,
-    182,   962,   -1202, -1474, 1468,  573,   -1325, 264,  383,   -829,  1458,
-    -1602, -130,  -681,  1017,  732,   608,   -1542, 411,  -205,  -1571, 1223,
-    652,   -552,  1015,  -1293, 1491,  -282,  -1544, 516,  -8,    -320,  -666,
-    -1618, -1162, 126,   1469,  -853,  -90,   -271,  830,  107,   -1421, -247,
-    -951,  -398,  961,   -1508, -725,  448,   -1065, 677,  -1275, -1103, 430,
-    555,   843,   -1251, 871,   1550,  105,   422,   587,  177,   -235,  -291,
-    -460,  1574,  1653,  -246,  778,   1159,  -147,  -777, 1483,  -602,  1119,
-    -1590, 644,   -872,  349,   418,   329,   -156,  -75,  817,   1097,  603,
-    610,   1322,  -1285, -1465, 384,   -1215, -136,  1218, -1335, -874,  220,
-    -1187, -1659, -1185, -1530, -1278, 794,   -1510, -854, -870,  478,   -108,
-    -308,  996,   991,   958,   -1460, 1522,  1628};
+  // Write input
+  for ( int i = 0; i < 256; i = i + 2 ) {
+    bit32_t input_word;
+    input_word( 15, 0 )  = poly[i + 0];
+    input_word( 31, 16 ) = poly[i + 1];
 
-sbit16_t montgomery_reduce(sbit32_t a);
-sbit16_t fqmul(sbit16_t a, sbit16_t b) {
-  return montgomery_reduce( (sbit32_t) a * b );
+    int nbytes = write(fdw, (void *)&input_word, sizeof(input_word));
+    assert(nbytes == sizeof(input_word));
+  }
+
+  // Read output
+  for ( int i = 0; i < 256; i = i + 2 ) {
+    bit32_t output_word;
+    int nbytes = read(fdr, (void *)&output_word, sizeof(output_word));
+    assert(nbytes == sizeof(output_word));
+
+    poly[i + 0] = output_word( 15, 0 );
+    poly[i + 1] = output_word( 31, 16 );
+  }
 }
-sbit16_t barrett_reduce(sbit16_t a);
-void ntt(sbit16_t r[256]);
-void invntt(sbit16_t r[256]);
+void invntt(sbit16_t r[256], int fdr, int fdw) {
+  // Write select word
+  bit32_t sel = 1;
+  int nbytes = write(fdw, (void *)&sel, sizeof(sel));
+  assert(nbytes == sizeof(sel));
+
+  // Write input
+  for ( int i = 0; i < 256; i = i + 2 ) {
+    bit32_t input_word;
+    input_word( 15, 0 )  = poly[i + 0];
+    input_word( 31, 16 ) = poly[i + 1];
+
+    int nbytes = write(fdw, (void *)&input_word, sizeof(input_word));
+    assert(nbytes == sizeof(input_word));
+  }
+
+  // Read output
+  for ( int i = 0; i < 256; i = i + 2 ) {
+    bit32_t output_word;
+    int nbytes = read(fdr, (void *)&output_word, sizeof(output_word));
+    assert(nbytes == sizeof(output_word));
+
+    poly[i + 0] = output_word( 15, 0 );
+    poly[i + 1] = output_word( 31, 16 );
+  }
+}
 
 /*************************************************
 * Name:        ntt_gold
@@ -102,7 +140,7 @@ void invntt_gold(sbit16_t r[256]) {
     r[j] = fqmul(r[j], f);
 }
 
-static int test_ntt(void)
+static int test_ntt(int fdr, int fdw)
 {
   bit8_t bytes[512];
   randombytes<sizeof(bytes)>(bytes);
@@ -118,7 +156,7 @@ static int test_ntt(void)
     r[i](15, 0) = r_gold[i];
   }
 
-  ntt(r);
+  ntt(r, fdr, fdw);
   ntt_gold(r_gold);
 
   for (int i = 0; i < 256; i++) {
@@ -131,7 +169,7 @@ static int test_ntt(void)
   return 0;
 }
 
-static int test_invntt(void)
+static int test_invntt(int fdr, int fdw)
 {
   bit8_t bytes[512];
   randombytes<sizeof(bytes)>(bytes);
@@ -147,7 +185,7 @@ static int test_invntt(void)
     r[i](15, 0) = r_gold[i];
   }
 
-  invntt(r);
+  invntt(r, fdr, fdw);
   invntt_gold(r_gold);
 
   for (int i = 0; i < 256; i++) {
@@ -165,9 +203,18 @@ int main()
   unsigned int i;
   int r;
 
+  int fdr = open("/dev/xillybus_read_32", O_RDONLY);
+  int fdw = open("/dev/xillybus_write_32", O_WRONLY);
+  
+  // Check that the channels are correctly opened
+  if ((fdr < 0) || (fdw < 0)) {
+    fprintf(stderr, "Failed to open Xillybus device channels\n");
+    exit(-1);
+  }
+
   for(i=0;i<NTESTS;i++) {
-    r  = test_ntt();
-    r |= test_invntt();
+    r  = test_ntt(fdr, fdw);
+    r |= test_invntt(fdr, fdw);
     if(r)
       return 1;
   }
