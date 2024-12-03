66feb8fde6272abc21e8f455ff10ba04470a74fc
acm289 <acm289@cornell.edu>
2024-12-03 00:48:13 -0500

    Worked on optimizing template

--- hls/ntt-fpga.cpp
+++ hls/ntt-fpga.cpp
@@ -1,6 +1,6 @@
 #include "params.h"
-#include "typedefs.h"
 #include "reduce.h"
+#include "typedefs.h"
 #include <hls_stream.h>
 
 /* Code to generate zetas and zetas_inv used in the number-theoretic
@@ -38,18 +38,19 @@ void init_ntt() {
 */
 
 const sbit16_t zetas[128] = {
-    -1044, -758,  -359,  -1517, 1493,  1422,  287,   202,  -171,  622,   1577,
-    182,   962,   -1202, -1474, 1468,  573,   -1325, 264,  383,   -829,  1458,
-    -1602, -130,  -681,  1017,  732,   608,   -1542, 411,  -205,  -1571, 1223,
-    652,   -552,  1015,  -1293, 1491,  -282,  -1544, 516,  -8,    -320,  -666,
-    -1618, -1162, 126,   1469,  -853,  -90,   -271,  830,  107,   -1421, -247,
-    -951,  -398,  961,   -1508, -725,  448,   -1065, 677,  -1275, -1103, 430,
-    555,   843,   -1251, 871,   1550,  105,   422,   587,  177,   -235,  -291,
-    -460,  1574,  1653,  -246,  778,   1159,  -147,  -777, 1483,  -602,  1119,
-    -1590, 644,   -872,  349,   418,   329,   -156,  -75,  817,   1097,  603,
-    610,   1322,  -1285, -1465, 384,   -1215, -136,  1218, -1335, -874,  220,
-    -1187, -1659, -1185, -1530, -1278, 794,   -1510, -854, -870,  478,   -108,
-    -308,  996,   991,   958,   -1460, 1522,  1628};
+    -1044, -758,  -359,  -1517, 1493,  1422,  287,   202,   -171,  622,
+    1577,  182,   962,   -1202, -1474, 1468,  573,   -1325, 264,   383,
+    -829,  1458,  -1602, -130,  -681,  1017,  732,   608,   -1542, 411,
+    -205,  -1571, 1223,  652,   -552,  1015,  -1293, 1491,  -282,  -1544,
+    516,   -8,    -320,  -666,  -1618, -1162, 126,   1469,  -853,  -90,
+    -271,  830,   107,   -1421, -247,  -951,  -398,  961,   -1508, -725,
+    448,   -1065, 677,   -1275, -1103, 430,   555,   843,   -1251, 871,
+    1550,  105,   422,   587,   177,   -235,  -291,  -460,  1574,  1653,
+    -246,  778,   1159,  -147,  -777,  1483,  -602,  1119,  -1590, 644,
+    -872,  349,   418,   329,   -156,  -75,   817,   1097,  603,   610,
+    1322,  -1285, -1465, 384,   -1215, -136,  1218,  -1335, -874,  220,
+    -1187, -1659, -1185, -1530, -1278, 794,   -1510, -854,  -870,  478,
+    -108,  -308,  996,   991,   958,   -1460, 1522,  1628 };
 
 /*************************************************
  * Name:        fqmul
@@ -78,19 +79,23 @@ static sbit16_t fqmul( sbit16_t a, sbit16_t b )
 template <int LEN, int K>
 void ntt_fpga_helper( sbit16_t r[256] )
 {
+#pragma HLS INLINE
   bit32_t  start, j, j_loop, k;
   sbit16_t t, zeta;
+  sbit16_t r_j, r_j_len;
 
   k = K;
   for ( start = 0; start < 256; start += ( 2 * LEN ) ) {
+#pragma HLS PIPELINE
     zeta = zetas[k++];
-    for ( j_loop = 0; j_loop < 128; j_loop++ ) {
-      j = j_loop + start;
-      if ( j < start + LEN ) {
-        t          = fqmul( zeta, r[j + LEN] );
-        r[j + LEN] = r[j] - t;
-        r[j]       = r[j] + t;
-      }
+    for ( j_loop = 0; j_loop < LEN; j_loop++ ) {
+      // #pragma HLS UNROLL
+      j          = j_loop + start;
+      r_j        = r[j];
+      r_j_len    = r[j + LEN];
+      t          = fqmul( zeta, r_j_len );
+      r[j + LEN] = r_j - t;
+      r[j]       = r_j + t;
     }
   }
 }
@@ -138,21 +143,24 @@ void ntt_fpga( sbit16_t r[256] )
 template <int LEN, int K>
 void invntt_fpga_helper( sbit16_t r[256] )
 {
+#pragma HLS INLINE
   bit32_t        start, j, j_loop, k;
   sbit16_t       t, zeta;
   const sbit16_t f = 1441;  // mont^2/128
+  sbit16_t       r_j, r_j_len;
 
   k = K;
   for ( start = 0; start < 256; start += ( 2 * LEN ) ) {
+#pragma HLS PIPELINE
     zeta = zetas[k--];
-    for ( j_loop = 0; j_loop < 128; j_loop++ ) {
-      j = j_loop + start;
-      if ( j < start + LEN ) {
-        t          = r[j];
-        r[j]       = barrett_reduce( t + r[j + LEN] );
-        r[j + LEN] = r[j + LEN] - t;
-        r[j + LEN] = fqmul( zeta, r[j + LEN] );
-      }
+    for ( j_loop = 0; j_loop < LEN; j_loop++ ) {
+      // #pragma HLS UNROLL
+      j          = j_loop + start;
+      r_j        = r[j];
+      r_j_len    = r[j + LEN];
+      r[j]       = barrett_reduce( r_j + r_j_len );
+      r_j_len    = r_j_len - r_j;
+      r[j + LEN] = fqmul( zeta, r_j_len );
     }
   }
 }
@@ -185,6 +193,7 @@ void invntt_fpga( sbit16_t r[256] )
   invntt_fpga_helper<128, 1>( r );
 
   for ( j = 0; j < 256; j++ )
+#pragma HLS PIPELINE
     r[j] = fqmul( r[j], f );
 }
 
@@ -198,9 +207,11 @@ void dut_ntt( hls::stream<bit32_t> &strm_in,
 {
   sbit16_t poly[256];
   bit32_t  sel_word = strm_in.read();
+#pragma HLS ARRAY_PARTITION variable = poly cyclic factor = 16 dim = 1
 
   // Read input
   for ( int i = 0; i < 256; i = i + 2 ) {
+#pragma HLS UNROLL
     bit32_t input_word;
     input_word = strm_in.read();
 
@@ -218,6 +229,7 @@ void dut_ntt( hls::stream<bit32_t> &strm_in,
 
   // Write resulting output
   for ( int i = 0; i < 256; i = i + 2 ) {
+#pragma HLS UNROLL
     bit32_t output_word;
     output_word( 15, 0 )  = poly[i + 0];
     output_word( 31, 16 ) = poly[i + 1];
